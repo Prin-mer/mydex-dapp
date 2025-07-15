0d89ad25658bfbcb388b12a6af4da1da3bfeb5a8
Prin-mer <mercyadaezeag@gmail.com>
2025-07-15 12:33:44 +0100

    Create tokens.js

---
+++ frontend/src/lib/tokens.js
@@ -0,0 +1,12 @@
+export const TOKENS = {
+  BNB: {
+    symbol: 'BNB',
+    address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // Native
+    decimals: 18
+  },
+  CAKE: {
+    symbol: 'CAKE',
+    address: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    decimals: 18
+  }
+};
