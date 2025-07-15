fb7183918d07ecec810e79d5dd94d10d1d88e857
Prin-mer <mercyadaezeag@gmail.com>
2025-07-15 12:53:37 +0100

    Update tokens.js

--- frontend/src/lib/tokens.js
+++ frontend/src/lib/tokens.js
@@ -4,14 +4,41 @@ export const tokenList = [
     address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
     isNative: true,
     decimals: 18,
-    logo: "https://assets.trustwalletapp.com/blockchains/binance/info/logo.png"
+    logo: "https://assets.trustwalletapp.com/blockchains/binance/info/logo.png",
   },
   {
     symbol: "CAKE",
     address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
     isNative: false,
     decimals: 18,
-    logo: "https://assets.trustwalletapp.com/blockchains/smartchain/assets/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82/logo.png"
+    logo: "https://assets.trustwalletapp.com/blockchains/smartchain/assets/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82/logo.png",
+  },
+  {
+    symbol: "BUSD",
+    address: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+    isNative: false,
+    decimals: 18,
+    logo: "https://assets.trustwalletapp.com/blockchains/smartchain/assets/0xe9e7cea3dedca5984780bafc599bd69add087d56/logo.png",
+  },
+  {
+    symbol: "USDT",
+    address: "0x55d398326f99059fF775485246999027B3197955",
+    isNative: false,
+    decimals: 18,
+    logo: "https://assets.trustwalletapp.com/blockchains/smartchain/assets/0x55d398326f99059fF775485246999027B3197955/logo.png",
+  },
+  {
+    symbol: "WBNB",
+    address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    isNative: false,
+    decimals: 18,
+    logo: "https://assets.trustwalletapp.com/blockchains/smartchain/assets/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c/logo.png",
+  },
+  {
+    symbol: "ADA",
+    address: "0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47",
+    isNative: false,
+    decimals: 18,
+    logo: "https://assets.trustwalletapp.com/blockchains/smartchain/assets/0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47/logo.png",
   }
 ];
-
