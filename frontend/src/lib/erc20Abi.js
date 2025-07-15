7d08590fd5bbb8963406e155054156362afb73c5
Prin-mer <mercyadaezeag@gmail.com>
2025-07-15 12:54:32 +0100

    Create erc20Abi.js

---
+++ frontend/src/lib/erc20Abi.js
@@ -0,0 +1,4 @@
+export const erc20Abi = [
+  "function approve(address spender, uint amount) external returns (bool)",
+  "function allowance(address owner, address spender) external view returns (uint)",
+];
