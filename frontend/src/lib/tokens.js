734c7df7dcc85f76983e8a9d71a152cfd9e75c76
Prin-mer <mercyadaezeag@gmail.com>
2025-07-15 12:46:59 +0100

    Update tokens.js

--- frontend/src/lib/tokens.js
+++ frontend/src/lib/tokens.js
@@ -1,12 +1,17 @@
-export const TOKENS = {
-  BNB: {
-    symbol: 'BNB',
-    address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // Native
-    decimals: 18
+export const tokenList = [
+  {
+    symbol: "BNB",
+    address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    isNative: true,
+    decimals: 18,
+    logo: "https://assets.trustwalletapp.com/blockchains/binance/info/logo.png"
   },
-  CAKE: {
-    symbol: 'CAKE',
-    address: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    decimals: 18
+  {
+    symbol: "CAKE",
+    address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
+    isNative: false,
+    decimals: 18,
+    logo: "https://assets.trustwalletapp.com/blockchains/smartchain/assets/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82/logo.png"
   }
-};
+];
+
